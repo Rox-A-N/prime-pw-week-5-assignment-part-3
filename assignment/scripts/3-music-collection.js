bcd4ae54a5ff7e1d132a7fdf9d08a279eb4a00e4
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-11-01 16:57:02 -0500

    added test console.logs to addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,14 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
-function addToCollection(){
+function addToCollection(title, artist, yearPublished){
+    return title + (' - ') + artist + (' - ') + yearPublished
+} // end addToCollection function
 
-};
+
+console.log('Add this album to my Collection ' ,addToCollection('Nevermind', 'Nirvana', 1991));
+
+
+
+
+console.log(collection.push(addToCollection));
